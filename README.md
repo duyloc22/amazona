7c9fe4778fa7a5494e2fe0fbaced8eb5acd8b55c
DuyLoc <phamndloc97@gmail.com>
2022-10-12 18:49:11 -0500

    Manage State By Reducer Hook

--- README.md
+++ README.md
@@ -41,4 +41,9 @@
 2. npm install axios
 3. use state hook
 4. use effect hook
-5. use reducer hook
+
+## Manage State By Reducer Hook
+
+1. define reducer
+2. update fetch data
+3. get state from useReducer
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import data from "../data";
+import logger from "use-reducer-logger";
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "FETCH_REQUEST":
+            return { ...state, loading: true };
+
+        case "FETCH_SUCCESS":
+            return { ...state, products: action.payload, loading: false };
+
+        case "FETCH_FAIL":
+            return { ...state, error: action.payload, loading: false };
+        default:
+            return state;
+    }
+};
 
 function HomeScreen() {
-    const [products, setProducts] = useState([]);
+    const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+        loading: true,
+        error: false,
+        products: [],
+    });
+    // const [products, setProducts] = useState([]);
     useEffect(() => {
         const fetchData = async () => {
-            const result = await axios.get("/api/products");
-            setProducts(result.data);
+            dispatch({ type: "FETCH_REQUEST" });
+            try {
+                const result = await axios.get("/api/products");
+                dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+            } catch (error) {
+                dispatch({ type: "FETCH_FAIL", payload: error });
+            }
+
+            // setProducts(result.data);
         };
         fetchData();
     }, []);
@@ -16,22 +43,28 @@ function HomeScreen() {
         <div>
             <h1>list product</h1>
             <div className="products">
-                {products.map((product) => (
-                    <div className="product" key={product.slug}>
-                        <Link to={`/product/${product.slug}`}>
-                            <img src={product.image} alt={product.name} />
-                        </Link>
-                        <div className="product-info">
+                {loading ? (
+                    <div>Loading...</div>
+                ) : error ? (
+                    <div>{error}</div>
+                ) : (
+                    products.map((product) => (
+                        <div className="product" key={product.slug}>
                             <Link to={`/product/${product.slug}`}>
-                                <p>{product.name}</p>
+                                <img src={product.image} alt={product.name} />
                             </Link>
-                            <p>
-                                <strong>${product.price}</strong>
-                            </p>
-                            <button>Add to cart</button>
+                            <div className="product-info">
+                                <Link to={`/product/${product.slug}`}>
+                                    <p>{product.name}</p>
+                                </Link>
+                                <p>
+                                    <strong>${product.price}</strong>
+                                </p>
+                                <button>Add to cart</button>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))
+                )}
             </div>
         </div>
     );
